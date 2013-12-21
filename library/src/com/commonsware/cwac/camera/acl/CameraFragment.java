c41eaa8e51e3d66cd6faed192b82d0c1929c9341
amond lee <amond@amond.net>
2013-12-22 07:04:37 +0900

    g

--- library/src/com/commonsware/cwac/camera/acl/CameraFragment.java
+++ library/src/com/commonsware/cwac/camera/acl/CameraFragment.java
@@ -17,17 +17,18 @@ package com.commonsware.cwac.camera.acl;
 import android.annotation.TargetApi;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import com.actionbarsherlock.app.SherlockFragment;
+
 import com.commonsware.cwac.camera.CameraHost;
 import com.commonsware.cwac.camera.CameraView;
 import com.commonsware.cwac.camera.SimpleCameraHost;
 import com.commonsware.cwac.camera.ZoomTransaction;
 
 @TargetApi(Build.VERSION_CODES.GINGERBREAD)
-public class CameraFragment extends SherlockFragment {
+public class CameraFragment extends Fragment {
   private CameraView cameraView=null;
   private CameraHost host=null;
 
